191d96c93dac7bd9b4360344106c8946f1b1aa16
robertFsouza <robertsouzarvj@gmail.com>
2022-01-02 14:05:49 -0300

    first commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+
+        gato gato = new gato();
+
+        System.out.println(gato);
+      /*  int a = 5;
+        int b = 3;
+        System.out.println("Hello world! " + (a+b));*/
+
+
+    }
+}
